7eedfe6a37fb4151ab3372058b482ce3d4aff36c
Sarah Elzinga <sevans924@gmail.com>
2019-03-22 15:40:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  if   number < 0 || number == 0 || number == 1
+        return false
+else
+  (2..(number - 1)).each do |n|
+     return false if number % n == 0
+
+end
+return true
+end
+
+
+
+end
