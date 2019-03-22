8861266a4d2a6c90a1893c24c39af233ff7abc1c
Sarah Elzinga <sevans924@gmail.com>
2019-03-22 15:07:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,14 @@
 def prime?(number)
+  if number > 0
   (2...number).each do |n|
     return false if number % n == 0
   end
       return true
+  else
+    (number...2).each do |n|
+      return false if number % n == 0
+    end
+    return true
+  end
   
 end# Add  code here!
