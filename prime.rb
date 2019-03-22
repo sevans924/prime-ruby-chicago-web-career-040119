d4762de041961941a0fdd6b6a5ec4901651c6539
Sarah Elzinga <sevans924@gmail.com>
2019-03-22 15:18:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,11 @@
 def prime?(number)
-  if number > 0
-  (1..number).each do |n|
+  if number < 0 || number == 0 || number == 1
+    return false
+  else
+  (2..number).each do |n|
     return false if number % n == 0
   end
       return true
-  elsif number < 0
-    (number..1).each do |n|
-      return false if number % n == 0
     end
-    return true
-  elsif number == 0
-    return false
-
-  end
 
 end# Add  code here!
